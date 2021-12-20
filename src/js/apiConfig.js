1c98073e25c5b428d253ab07ace318dbbf1ab876
Karolinasjokvist <83401567+Karolinasjokvist@users.noreply.github.com>
2021-12-20 01:38:51 +0100

    Remove ignored files

--- src/js/apiConfig.js
+++
@@ -1,2 +0,0 @@
-export const BASE_URL = "https://deezerdevs-deezer.p.rapidapi.com";
-export const API_KEY = "0e7a2419a6msh36d6be08a4acd94p1bf6b3jsndf277dade78a";
--- src/js/firebaseConfig.js
+++
@@ -1,6 +0,0 @@
-import { initializeApp } from 'firebase/app';
-import { firebaseConfig } from './firebasecd';
-import { getFirestore } from 'firebase/firestore';
-
-export const app = initializeApp(firebaseConfig);
-export const db = getFirestore();
